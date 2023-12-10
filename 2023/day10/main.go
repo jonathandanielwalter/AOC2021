4385e82a7ce41deaaf96bb8814d0146fa8d86c7d
Jonny Walter <jonathandanielwalter@gmail.com>
2023-12-10 20:42:08 +0000

    wip part 2

--- 2023/day10/main.go
+++ 2023/day10/main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"strings"
 )
 
 func main() {
-	file, err := os.Open("input.txt")
+	file, err := os.Open("testinput.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	partOne(scanner)
+	run(scanner)
 
 }
 
@@ -53,8 +54,11 @@ type coord struct {
 
 var rows [][]string
 var loopLength int
+var loopCoordinates map[string]string
 
-func partOne(scanner *bufio.Scanner) {
+func run(scanner *bufio.Scanner) {
+	loopCoordinates = map[string]string{}
+
 	for scanner.Scan() {
 		rows = append(rows, strings.Split(scanner.Text(), ""))
 	}
@@ -70,7 +74,6 @@ func partOne(scanner *bufio.Scanner) {
 	var distanceTravelled int
 
 	for true {
-
 		coords := neighborCoordinates(currentTile.x, currentTile.y)
 		for _, c := range coords {
 			currentTileValue := rows[currentTile.y][currentTile.x]
@@ -86,12 +89,14 @@ func partOne(scanner *bufio.Scanner) {
 					continue
 				}
 				if isFinalConnection(c.fromDirection, c.toDirection, currentTileValue) {
+					addCoordinateToMap(currentTile)
 					loopLength = distanceTravelled
 					currentTile = c
 					break
 				}
 			} else {
 				if !(c.x == previousTile.x && c.y == previousTile.y) && isConnectingPipe(c.fromDirection, c.toDirection, adjacentTile, currentTileValue) {
+					addCoordinateToMap(currentTile)
 					previousTile = currentTile
 					currentTile = c
 					distanceTravelled++
@@ -105,8 +110,95 @@ func partOne(scanner *bufio.Scanner) {
 		}
 	}
 
-	log.Println(loopLength/2 + 1)
+	numberInside := 0
 
+	//
+
+	// for y, row := range rows {
+	// 	firstReached := false
+	// 	threshholdsCrossed := 0
+	// 	for x, _ := range row {
+	// 		coordString := fmt.Sprintf("%s,%s", fmt.Sprint(x), fmt.Sprint(y))
+	// 		previousCoord := fmt.Sprintf("%s,%s", fmt.Sprint(x-1), fmt.Sprint(y))
+	// 		if _, ok := loopCoordinates[coordString]; ok {
+	// 			if !firstReached {
+	// 				threshholdsCrossed++
+	// 			} else {
+	// 				//if this pipe is connected to the last pipe, we dont count it as a threshhold crossed
+	// 				if x != 0 {
+	// 					if _, ok := loopCoordinates[previousCoord]; ok {
+
+	// 						previousTile := rows[y][x-1]
+	// 						currentTile := rows[y][x]
+	// 						nextTile := rows[y][x+1]
+	// 						if isPipeConnectedBothWays("W", "E", currentTile, previousTile, nextTile) {
+	// 							continue //ignore this tile
+	// 						}else{
+	// 							threshholdsCrossed++
+	// 						}
+
+	// 					}else{
+	// 						threshholdsCrossed++
+	// 					}
+	// 				}
+	// 			}
+	// 		} else {
+	// 			if threshholdsCrossed != 0 && threshholdsCrossed%2 == 1 {
+	// 				numberInside++
+	// 			}
+	// 			// if _, ok := loopCoordinates[previousCoord]; ok {
+	// 			// 	threshholdsCrossed++
+	// 			// }
+
+	// 		}
+	// 	}
+	// }
+
+	var cleanRows [][]string
+
+	for _, row := range rows{
+		var tiles []string
+		for i:=0; i < len(row); i++{
+			tiles = append(tiles, ".")
+		}
+		cleanRows = append(cleanRows, tiles)
+	}
+
+	for k, v := range loopCoordinates{
+		
+	}
+
+	for y, row := range rows {
+		threshholdsCrossed := 0
+		for x, _ := range row {
+			coordString := fmt.Sprintf("%s,%s", fmt.Sprint(x), fmt.Sprint(y))
+			if _, ok := loopCoordinates[coordString]; ok {
+				continue
+			}
+			for i := x; i < len(row); i++ {
+				coordString := fmt.Sprintf("%s,%s", fmt.Sprint(i), fmt.Sprint(y))
+				if _, ok := loopCoordinates[coordString]; ok {
+					if row[i] == "-" {
+						continue
+					} else {
+						threshholdsCrossed++
+					}
+				}
+			}
+			if threshholdsCrossed != 0 && threshholdsCrossed%2 == 1 {
+				numberInside++
+			}
+			threshholdsCrossed = 0
+		}
+	}
+
+	log.Println("part 1", loopLength/2+1)
+	log.Println("part 2", numberInside)
+}
+
+func addCoordinateToMap(c coord) {
+	coordString := fmt.Sprintf("%s,%s", fmt.Sprint(c.x), fmt.Sprint(c.y))
+	loopCoordinates[coordString] = true
 }
 
 func isConnectingPipe(fromDirection string, toDirection string, destinationTile string, originTile string) bool {
@@ -145,6 +237,25 @@ func isFinalConnection(fromDirection string, toDirection string, originTile stri
 	}
 
 	return false
+}
+
+func isPipeConnectedBothWays(fromDirection string, toDirection string, currentTile string, previousTile string, nextTile string) bool {
+	var left bool
+	var right bool
+	for _, direction := range pipeDirectory[currentTile].connectingDirections {
+		if direction == fromDirection {
+			left = true
+		}
+
+	}
+	for _, direction := range pipeDirectory[nextTile].connectingDirections {
+		if direction == fromDirection {
+			right = true
+		}
+
+	}
+
+	return left && right
 }
 
 // only cares about directly, non diagonal neighbours
