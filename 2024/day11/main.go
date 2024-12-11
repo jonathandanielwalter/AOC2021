c5b4d5e1365ff90fbdd786658e6a3938cfe83e2d
jonathandanielwalter <jonathandanielwalter@gmail.com>
2024-12-11 10:04:50 +0000

    aoc

--- 2024/day11/main.go
+++ 2024/day11/main.go
@@ -47,6 +47,8 @@ func part1(inputs []uint64) int {
 }
 
 func part2(inputs []uint64) int {
+	results := map[uint64][]uint64{}
+
 	stones := map[uint64]int{}
 	for _, in := range inputs {
 		stones[in]++
@@ -57,21 +59,33 @@ func part2(inputs []uint64) int {
 		for key, input := range stones {
 
 			for i := 0; i < input; i++ {
-				switch {
-				case key == 0:
-					amended[1]++
-				case len(strconv.FormatUint(key, 10))%2 == 0:
-					str := strconv.FormatUint(key, 10)
-					a := str[:len(str)/2]
-					b := str[len(str)/2:]
-					ai, _ := strconv.ParseUint(a, 10, 64)
-					bi, _ := strconv.ParseUint(b, 10, 64)
-					amended[ai]++
-					amended[bi]++
+				if val, ok := results[key]; ok {
+					for _, v := range val {
+						amended[v]++
+					}
+				} else {
+					switch {
+					case key == 0:
+						amended[1]++
+						results[key] = []uint64{1}
 
-				default:
-					amended[key*2024]++
+					case len(strconv.FormatUint(key, 10))%2 == 0:
+						str := strconv.FormatUint(key, 10)
+						a := str[:len(str)/2]
+						b := str[len(str)/2:]
+						ai, _ := strconv.ParseUint(a, 10, 64)
+						bi, _ := strconv.ParseUint(b, 10, 64)
+						amended[ai]++
+						amended[bi]++
+
+						results[key] = []uint64{ai, bi}
+
+					default:
+						amended[key*2024]++
+						results[key] = []uint64{key * 2024}
+					}
 				}
+
 			}
 
 		}
